3452d57e2db261390c6fc7badbdd6bf9039c70b2
aiyo <'aiyo407@163.com'>
2019-03-16 15:32:04 +0800

    以日志形式记录每个请求信息

--- familytree/src/main/java/com/starfire/familytree/config/ApplicationConfig.java
+++ familytree/src/main/java/com/starfire/familytree/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
@@ -20,5 +21,16 @@ public class ApplicationConfig {
 	         
 	        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
 	        return eventMulticaster;
+	    }
+	  @Bean
+	    public CommonsRequestLoggingFilter logFilter() {
+	        CommonsRequestLoggingFilter filter
+	          = new CommonsRequestLoggingFilter();
+	        filter.setIncludeQueryString(true);
+	        filter.setIncludePayload(true);
+	        filter.setMaxPayloadLength(10000);
+	        filter.setIncludeHeaders(false);
+	        filter.setAfterMessagePrefix("REQUEST DATA : ");
+	        return filter;
 	    }
 }
