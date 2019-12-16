ff00f87b1b4c05b2e61281942a1ad834c7de37cf
luzh <luzhihua407@126.com>
2019-12-16 08:47:02 +0800

    用戶權限

--- familytree/src/main/java/com/starfire/familytree/excel/controller/ExcelController.java
+++ familytree/src/main/java/com/starfire/familytree/excel/controller/ExcelController.java
@@ -4,6 +4,8 @@ package com.starfire.familytree.excel.controller;
 import com.starfire.familytree.excel.service.IExcelService;
 import com.starfire.familytree.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.io.*;
 
 /**
@@ -33,6 +37,16 @@ public class ExcelController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Response<String> response = new Response<>();
+        return response.success("");
+    }
+
+    @PostMapping(value="/toPDF")
+    public Response<String> toPDF() throws PrinterException {
+        PrinterJob job = PrinterJob.getPrinterJob();
+        PDDocument document=new PDDocument();
+        job.setPageable(new PDFPageable(document));
+        job.print();
         Response<String> response = new Response<>();
         return response.success("");
     }
